e0713d955acff2e2e653578b15cfaa8cd76d1662
vagrant <vagrant@localhost.localdomain>
2020-05-18 11:09:32 +0000

    [いいね機能追加]課題3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :trackable,:validatable
 
   has_many :books
+  has_many :favorites, dependent:  :destroy
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, presence:true, length:{maximum:20,minimum:2}
   validates :introduction, length:{maximum:50}
+
+  def already_favorited?(book)
+    self.favorites.exists?(book_id: book.id)
+  end
 end

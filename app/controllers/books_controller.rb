486ffc847c4111a20eaecef69374e0551826407f
vagrant <vagrant@localhost.localdomain>
2020-05-19 06:18:34 +0000

    コメント機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   def show
 		@book = Book.find(params[:id])
 		@user = @book.user
-		@book_new = Book.new
+    @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
 	def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
 	belongs_to :user
-	has_many :favorites, dependent:  :destroy
+	has_many :favorites, dependent:  :destroy #bookが削除されるといいねも削除
+	has_many :book_comments, dependent:  :destroy #bookが削除されるとコメントも削除
 	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
 	#presence trueは空欄の場合を意味する。
 	validates :title, presence:true

b68c5be6798ebc354803f8bb66e30924ed763e4a
ryu2129 <ryu212920@gmail.com>
2020-06-16 14:46:42 +0000

    add ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,16 @@
 class BookCommentsController < ApplicationController
+	before_action :authenticate_user!
+
 	def create
 		book = Book.find(params[:book_id])
+		book_new = Book.new
 		comment = current_user.book_comments.new(book_comment_params)
-		comment.book_id = book.id
-		comment.save
-		redirect_back(fallback_location: root_path)
+		comment.user_id = current_user.id
+		if comment.save
+			flash[:success] = "Comment was successfully created."
+		else
+			book_comments = BookComment.where(:id book)
+		end
 	end
 
   def destroy
